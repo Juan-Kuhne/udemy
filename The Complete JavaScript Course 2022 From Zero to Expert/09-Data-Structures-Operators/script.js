3c4728be93f85e22b42a48e3a107c56c5ce3686b
Lucris <juankuhne@hotmail.com>
2022-02-07 09:49:44 -0300

    The Spread Operator

--- The Complete JavaScript Course 2022 From Zero to Expert/09-Data-Structures-Operators/script.js
+++ The Complete JavaScript Course 2022 From Zero to Expert/09-Data-Structures-Operators/script.js
@@ -34,9 +34,53 @@ const restaurant = {
    orderDelivery: function ({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
       console.log(`Order recieved! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
    },
+
+   orderPasta: function (ing1, ing2, ing3) {
+      console.log(`Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`);
+   },
 };
 
-restaurant.orderDelivery({
+const arr = [7, 8, 9];
+const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewArr);
+
+const goodNewArr = [1, 2, ...arr];
+console.log(goodNewArr);
+
+console.log(...goodNewArr);
+console.log(1, 2, 7, 8, 9);
+
+const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+console.log(newMenu);
+
+// Copy array
+const mainMenuCopy = [...restaurant.mainMenu];
+
+// Join 2 arrays
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(menu);
+
+// Iterables: arrays, strings, maps, sets. NOT objects
+const str = 'Jonas';
+const letters = [...str, ' ', 'S.'];
+console.log(letters);
+
+// Real world example
+// const ingredients = [prompt("Let's make pasta! Ingredient 1?"), prompt('Ingredient 2?'), prompt('Ingredient 3?')];
+// console.log(ingredients);
+
+// restaurant.orderPasta(...ingredients);
+
+// Objects
+const newRestaurant = { foundedIn: 1998, ...restaurant, founder: 'Guiseppe' };
+console.log(newRestaurant);
+
+const restaurantCopy = { ...restaurant };
+restaurantCopy.name = 'Ristorante Roma';
+console.log(restaurantCopy.name);
+console.log(restaurant.name);
+
+/* restaurant.orderDelivery({
    time: '22:30',
    address: 'Via del sole, 21',
    mainIndex: 2,
@@ -69,7 +113,7 @@ console.log(a, b);
 const {
    fri: { open: o, close: c },
 } = openingHours;
-console.log(o, c);
+console.log(o, c); */
 
 /* const arr = [2, 3, 4];
 const a = arr[0];
