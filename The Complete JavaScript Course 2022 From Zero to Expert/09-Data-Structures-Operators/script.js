83df6dd709692550a145490ea94f2ef22c76540e
Lucris <juankuhne@hotmail.com>
2022-02-07 10:17:35 -0300

    Rest pattern

--- The Complete JavaScript Course 2022 From Zero to Expert/09-Data-Structures-Operators/script.js
+++ The Complete JavaScript Course 2022 From Zero to Expert/09-Data-Structures-Operators/script.js
@@ -38,8 +38,48 @@ const restaurant = {
    orderPasta: function (ing1, ing2, ing3) {
       console.log(`Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`);
    },
+
+   orderPizza: function (mainIngredient, ...otherIngredients) {
+      console.log(mainIngredient);
+      console.log(otherIngredients);
+   },
 };
 
+// 1) Destructurig
+
+// SPREAD, because on RIGHT side of =
+const arr = [1, 2, ...[3, 4]];
+
+// REST, because on LEFT side of =
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
+
+const [pizza, , risotto, ...otherFood] = [...restaurant.mainMenu, ...restaurant.starterMenu];
+console.log(pizza, risotto, otherFood);
+
+// Objects
+const { sat, ...weekdays } = { ...restaurant.openingHours };
+console.log(sat, weekdays);
+
+// 2) Functions
+const add = function (...numbers) {
+   let sum = 0;
+   numbers.forEach(num => (sum += num));
+   console.log(sum);
+};
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+
+const x = [23, 5, 7];
+add(...x);
+
+restaurant.orderPizza('mushroms', 'onions', 'olives', 'spinach');
+restaurant.orderPizza('mushroms');
+
+/* 
+/////////////////////////////////////////////////////
+// The Spread Operator
 const arr = [7, 8, 9];
 const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
 console.log(badNewArr);
@@ -79,6 +119,7 @@ const restaurantCopy = { ...restaurant };
 restaurantCopy.name = 'Ristorante Roma';
 console.log(restaurantCopy.name);
 console.log(restaurant.name);
+ */
 
 /* restaurant.orderDelivery({
    time: '22:30',
