9c465c9184a7bc4965d631ae104393b0d659ce72
Lucris <juankuhne@hotmail.com>
2022-02-07 10:41:27 -0300

    Short circuiting (&& and ||)

--- The Complete JavaScript Course 2022 From Zero to Expert/09-Data-Structures-Operators/script.js
+++ The Complete JavaScript Course 2022 From Zero to Expert/09-Data-Structures-Operators/script.js
@@ -45,6 +45,37 @@ const restaurant = {
    },
 };
 
+console.log('---- OR ----');
+// Use ANY, data type, return ANY data type, short-circuiting
+console.log(3 || 'Jonas');
+console.log('' || 'Jonas');
+console.log(true || 0);
+console.log(undefined || null);
+
+console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+
+restaurant.numGuests = 0;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+
+console.log('---- AND ----');
+console.log(0 && 'Jonas');
+console.log(7 && 'Jonas');
+
+console.log('Hello' && 23 && null && 'Jonas');
+
+// Practical example
+if (restaurant.orderPizza) {
+   restaurant.orderPizza('mushrooms', 'spinach');
+}
+restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
+
+/* 
+//////////////////////////////////////////////////
+// Rest pattern and parameters
 // 1) Destructurig
 
 // SPREAD, because on RIGHT side of =
@@ -76,6 +107,7 @@ add(...x);
 
 restaurant.orderPizza('mushroms', 'onions', 'olives', 'spinach');
 restaurant.orderPizza('mushroms');
+ */
 
 /* 
 /////////////////////////////////////////////////////
