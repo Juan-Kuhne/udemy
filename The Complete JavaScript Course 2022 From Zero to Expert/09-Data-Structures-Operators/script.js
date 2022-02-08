71134661f9ebf1ea5b0d25f616696dd1d7953764
Lucris <juankuhne@hotmail.com>
2022-02-08 14:01:56 -0300

    Maps: Iteration

--- The Complete JavaScript Course 2022 From Zero to Expert/09-Data-Structures-Operators/script.js
+++ The Complete JavaScript Course 2022 From Zero to Expert/09-Data-Structures-Operators/script.js
@@ -49,6 +49,42 @@ const restaurant = {
    },
 };
 
+const question = new Map([
+   ['question', 'What is the best programming language in the world?'],
+   [1, 'C'],
+   [2, 'Java'],
+   [3, 'JavaScript'],
+   ['correct', 3],
+   [true, 'Correct 🎉'],
+   [false, 'Try again!'],
+]);
+
+console.log(question);
+
+// Convert object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+// Quiz app
+console.log('%cQuiz App', 'color: green');
+console.log(question.get('question'));
+for (const [key, value] of question) {
+   if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+const answer = Number(prompt('Your answer'));
+console.log(answer);
+console.log(question.get(answer === question.get('correct')));
+
+// Convert map to array
+console.log([...question]);
+console.log(question.entries());
+console.log(...question.keys());
+console.log(question.values());
+
+/* 
+///////////////////////////////////////////////////////////
+// Maps: fundamentals
 const rest = new Map();
 rest.set('name', 'Classico Italiano');
 rest.set(1, 'Firenze, Italy');
@@ -80,7 +116,7 @@ const arr = [1, 2];
 rest.set(arr, 'Test');
 rest.set(document.querySelector('h1'), 'Heading');
 console.log(rest);
-console.log(rest.get(arr));
+console.log(rest.get(arr)); */
 
 /* 
 //////////////////////////////////////////////////////////////////////////////////
