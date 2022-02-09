ded1a5b5f072ee7dcddbfe71b088208aebb71654
Lucris <juankuhne@hotmail.com>
2022-02-09 14:14:12 -0300

    Working with strings - part 3

--- The Complete JavaScript Course 2022 From Zero to Expert/09-Data-Structures-Operators/script.js
+++ The Complete JavaScript Course 2022 From Zero to Expert/09-Data-Structures-Operators/script.js
@@ -49,6 +49,59 @@ const restaurant = {
    },
 };
 
+console.log('a+very+nice+string'.split('+'));
+console.log('Jonas Schmedtmann'.split(' '));
+
+const [firstName, lastName] = 'Jonas Schmedtmann'.split(' ');
+console.log(firstName, lastName);
+
+const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
+console.log(newName);
+
+const capitalizeName = function (name) {
+   const names = name.split(' ');
+   const namesUpper = [];
+
+   for (const n of names) {
+      // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+      namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+   }
+   console.log(namesUpper.join(' '));
+};
+
+capitalizeName('jessica ann smith davis');
+capitalizeName('jonas schmedtmann');
+
+// Padding
+const message = 'Go to gate 23!';
+console.log(message.padStart(20, '+').padEnd(30, '+'));
+console.log('Jonas'.padStart(20, '+').padEnd(30, '+'));
+
+const maskCreditCard = function (number) {
+   const str = number + ''; // convert to string
+   const last = str.slice(-4);
+   console.log(last.padStart(str.length, '*'));
+};
+
+maskCreditCard(35465454);
+maskCreditCard(435135168432168);
+maskCreditCard('6513568163546546547654684');
+
+// Repeat
+const message2 = 'Bad wheather... All departues delayed... ';
+console.log(message2.repeat(5));
+
+const planesInLine = function (n) {
+   console.log(`There are ${n} planes in line \n${'✈'.repeat(n)}`);
+};
+
+planesInLine(5);
+planesInLine(3);
+planesInLine(12);
+
+/* 
+//////////////////////////////////////////////////////////////////////////////
+// Working with strings - part 2
 const airline = 'TAP Air Portugal';
 
 console.log(airline.toLowerCase());
@@ -99,7 +152,7 @@ const checkBaggage = function (items) {
 
 checkBaggage('I have a laptop, some Food and a pocket Knife');
 checkBaggage('Socks and camera');
-checkBaggage('Got some snacks and a gun for protection');
+checkBaggage('Got some snacks and a gun for protection'); */
 
 /* 
 //////////////////////////////////////////////////////
