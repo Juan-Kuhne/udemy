a3b4387a05fab8cc5354aa92789ef95f7e37c30e
Lucris <juankuhne@hotmail.com>
2022-02-10 13:49:23 -0300

    The call and apply methods

--- The Complete JavaScript Course 2022 From Zero to Expert/10-Functions/script.js
+++ The Complete JavaScript Course 2022 From Zero to Expert/10-Functions/script.js
@@ -87,6 +87,9 @@ document.body.addEventListener('click', high5);
 
 ['Jonas', 'Martha', 'Adam'].forEach(high5); */
 
+/* 
+////////////////////////////////////////////////////////////
+// Functions returning functions
 // const greet = function (greeting) {
 //    return function (name) {
 //       console.log(`${greeting} ${name}`);
@@ -100,4 +103,56 @@ const greeterHey = greet('Hey');
 greeterHey('Jonas');
 greeterHey('Steven');
 
-greet('Hello')('Jonas');
+greet('Hello')('Jonas'); */
+
+const lufthansa = {
+   airline: 'Lufthansa',
+   iataCode: 'LH',
+   bookings: [],
+   // book: function() {}
+   book(flightNum, name) {
+      console.log(`${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`);
+      this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+   },
+};
+
+lufthansa.book(239, 'Jonas Schmedtmann');
+lufthansa.book(635, 'John Smith');
+console.log(lufthansa);
+
+const eurowings = {
+   name: 'Eurowings',
+   iataCode: 'EW',
+   bookings: [],
+};
+
+const book = lufthansa.book;
+
+// Does not work
+// book(23, 'Sara Willians');
+
+//Call method
+book.call(eurowings, 23, 'Sarah Williams');
+console.log(eurowings);
+
+book.call(lufthansa, 239, 'Mary Cooper');
+console.log(lufthansa);
+
+lufthansa.book.call(lufthansa, 66, 'Bigaill');
+console.log(lufthansa);
+
+const swiss = {
+   airline: 'Swiss Air Lines',
+   iataCode: 'LX',
+   bookings: [],
+};
+
+book.call(swiss, 583, 'Mary Cooper');
+console.log(swiss);
+
+// Apply method
+const flightData = [583, 'George Cooper'];
+book.apply(swiss, flightData);
+console.log(swiss);
+
+book.call(swiss, ...flightData);
