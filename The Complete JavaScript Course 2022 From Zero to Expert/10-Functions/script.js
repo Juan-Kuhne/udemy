629ea65d97632a4a6491398caac5e12db0bea81c
Lucris <juankuhne@hotmail.com>
2022-02-10 10:09:09 -0300

    Passing arguments: value x reference

--- The Complete JavaScript Course 2022 From Zero to Expert/10-Functions/script.js
+++ The Complete JavaScript Course 2022 From Zero to Expert/10-Functions/script.js
@@ -1,5 +1,8 @@
 'use strict';
 
+/* 
+///////////////////////////////////////////////////////////////
+// Default parameters
 const bookings = [];
 
 const createBooking = function (flightNum, numPassengers = 1, price = 199 * numPassengers) {
@@ -21,4 +24,32 @@ createBooking('LH123', 2, 800);
 createBooking('LH123', 2);
 createBooking('LH123', 5);
 
-createBooking('LH123', undefined, 1000);
+createBooking('LH123', undefined, 1000); */
+
+const flight = 'LH234';
+const jonas = {
+   name: 'Jonas Schmedtmann',
+   passport: 24739479284,
+};
+
+const checkIn = function (flightNum, passenger) {
+   flightNum = 'LH999';
+   passenger.name = 'Mr.' + passenger.name;
+
+   if (passenger.passport === 24739479284) {
+      alert('Check in');
+   } else {
+      alert('Wrong passport!');
+   }
+};
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+const newPassport = function (person) {
+   person.passport = Math.trunc(Math.random() * 100000000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
