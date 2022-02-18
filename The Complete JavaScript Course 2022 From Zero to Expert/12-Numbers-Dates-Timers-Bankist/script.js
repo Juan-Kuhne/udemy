5502037d9e0b79d58485c5f47796c19eee522da2
Lucris <juankuhne@hotmail.com>
2022-02-18 09:35:53 -0300

    The remainder operator

--- The Complete JavaScript Course 2022 From Zero to Expert/12-Numbers-Dates-Timers-Bankist/script.js
+++ The Complete JavaScript Course 2022 From Zero to Expert/12-Numbers-Dates-Timers-Bankist/script.js
@@ -265,6 +265,7 @@ console.log(Number.isFinite(23 / 0));
 console.log(Number.isInteger(23));
 console.log(Number.isInteger(23.0)); */
 
+/* 
 /////////////////////////////////////////////////
 // Converting and checking numbers
 
@@ -304,4 +305,31 @@ console.log(Math.floor(-23.9));
 console.log((2.7).toFixed(0));
 console.log((2.7).toFixed(3));
 console.log((2.345).toFixed(2));
-console.log(+(2.345).toFixed(2));
+console.log(+(2.345).toFixed(2)); */
+
+/////////////////////////////////////////////////
+// The remainder operator
+
+console.log(5 % 2);
+console.log(5 / 2); // 5 = 2 * 2 + 1
+
+console.log(8 % 3);
+console.log(8 / 3); // 8 = 2 * 3 + 2
+
+console.log(6 % 2);
+console.log(6 / 2);
+
+console.log(7 % 2);
+console.log(7 / 2);
+
+const isEven = n => n % 2 === 0;
+console.log(isEven(8));
+console.log(isEven(23));
+console.log(isEven(514));
+
+labelBalance.addEventListener('click', function (e) {
+   [...document.querySelectorAll('.movements__row')].forEach((row, i) => {
+      if (i % 2 === 0) row.style.backgroundColor = '#eee';
+      if (i % 3 === 0) row.style.backgroundColor = '#466';
+   });
+});
