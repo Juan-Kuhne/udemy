721e32f6bfe568268b12b6efcb23907418981842
Lucris <juankuhne@hotmail.com>
2022-02-23 10:30:26 -0300

    Smooth scrolling

--- The Complete JavaScript Course 2022 From Zero to Expert/13-Advanced-DOM-Bankist/script.js
+++ The Complete JavaScript Course 2022 From Zero to Expert/13-Advanced-DOM-Bankist/script.js
@@ -81,6 +81,7 @@ console.log(getComputedStyle(message).height);
 
 message.style.height = Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
 
+/* 
 document.documentElement.style.setProperty('--color-primary', 'orangered');
 
 // Attributes
@@ -112,4 +113,30 @@ logo.classList.toggle('c');
 logo.classList.contains('c'); // not includes
 
 // Don't use
-logo.className = 'jonas';
+logo.className = 'jonas'; */
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+   const s1coords = section1.getBoundingClientRect();
+   console.log(s1coords);
+
+   console.log(e.target.getBoundingClientRect());
+
+   console.log('Current scroll (X/Y): ', window.pageXOffset, window.pageYOffset);
+
+   console.log('height/width viewport', document.documentElement.clientHeight, document.documentElement.clientWidth);
+
+   // Scrolling
+
+   // window.scrollTo(s1coords.left + window.pageXOffset, s1coords.top + window.pageYOffset);
+
+   // window.scrollTo({
+   //    left: s1coords.left + window.pageXOffset,
+   //    top: s1coords.top + window.pageYOffset,
+   //    behavior: 'smooth',
+   // });
+
+   section1.scrollIntoView({ behavior: 'smooth' });
+});
