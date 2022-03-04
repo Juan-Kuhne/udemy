38d09a300d26193a59943ef24011db614946b229
Lucris <juankuhne@hotmail.com>
2022-03-04 14:23:14 -0300

    Inheritance between "Classes": Object.create

--- The Complete JavaScript Course 2022 From Zero to Expert/14-OOP/script.js
+++ The Complete JavaScript Course 2022 From Zero to Expert/14-OOP/script.js
@@ -222,6 +222,7 @@ console.log(mike instanceof Object);
 Student.prototype.constructor = Student;
 console.dir(Student.prototype.constructor); */
 
+/* 
 /////////////////////////////////////////////////////
 // Inheritance between "Classes": ES6 Classes
 class PersonCl {
@@ -277,4 +278,34 @@ class StudenCl extends PersonCl {
 
 const martha = new StudenCl('Martha Jones', 2012, 'Computer Science');
 martha.introduce();
-martha.calcAge();
+martha.calcAge(); */
+
+/////////////////////////////////////////////////////
+// Inheritance between "Classes": Object.create
+const PersonProto = {
+   calcAge() {
+      console.log(2037 - this.birthYear);
+   },
+
+   init(firstName, birthYear) {
+      this.firstName = firstName;
+      this.birthYear = birthYear;
+   },
+};
+
+const steven = Object.create(PersonProto);
+
+const StudentProto = Object.create(PersonProto);
+StudentProto.init = function (firstName, birthYear, course) {
+   PersonProto.init.call(this, firstName, birthYear);
+   this.course = course;
+};
+
+StudentProto.introduce = function () {
+   console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const jay = Object.create(StudentProto);
+jay.init('Jay', 2010, 'Computer Science');
+jay.introduce();
+jay.calcAge();
