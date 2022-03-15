ecf8a6ec2c91baa63fe00bb0bec777f32c46b8bb
Juan Kuhne <juan.kuhne@certisign.com.br>
2022-03-15 14:38:20 -0300

    Encapsulation: Protected properties and methods

--- The Complete JavaScript Course 2022 From Zero to Expert/14-OOP/script.js
+++ The Complete JavaScript Course 2022 From Zero to Expert/14-OOP/script.js
@@ -280,6 +280,7 @@ const martha = new StudenCl('Martha Jones', 2012, 'Computer Science');
 martha.introduce();
 martha.calcAge(); */
 
+/* 
 /////////////////////////////////////////////////////
 // Inheritance between "Classes": Object.create
 const PersonProto = {
@@ -308,4 +309,57 @@ StudentProto.introduce = function () {
 const jay = Object.create(StudentProto);
 jay.init('Jay', 2010, 'Computer Science');
 jay.introduce();
-jay.calcAge();
+jay.calcAge(); */
+
+/////////////////////////////////////////////////////
+// Encapsulation
+
+class Account {
+   constructor(owner, currency, pin) {
+      this.owner = owner;
+      this.currency = currency;
+      // Protected property
+      this._pin = pin;
+      this._movements = [];
+      this.locale = navigator.language;
+
+      console.log(`Thanks for opening an account, ${owner}`);
+   }
+
+   // Public interface
+   getMovements() {
+      return this._movements;
+   }
+
+   deposit(val) {
+      this._movements.push(val);
+   }
+
+   withdraw(val) {
+      this.movements.push(-val);
+   }
+
+   _approveLoan(val) {
+      return true;
+   }
+
+   requestLoan(val) {
+      if (this.approveLoan(val)) {
+         this.deposit(val);
+         console.log(`Loan approved`);
+      }
+   }
+}
+
+const acc1 = new Account('Jonas', 'EUR', 1111);
+
+// acc1.movements.push(250);
+// acc1.movements.push(-140);
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+acc1.approveLoan(1000);
+console.log(acc1.getMovements());
+
+console.log(acc1);
+console.log(acc1.pin);
