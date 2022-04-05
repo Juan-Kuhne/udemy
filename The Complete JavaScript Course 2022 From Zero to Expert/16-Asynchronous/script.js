4c23d045a46d7e19fca43a625cf39e852650a807
Juan Kuhne <juan.kuhne@certisign.com.br>
2022-04-05 14:26:26 -0300

    Chaining rejected promises

--- The Complete JavaScript Course 2022 From Zero to Expert/16-Asynchronous/script.js
+++ The Complete JavaScript Course 2022 From Zero to Expert/16-Asynchronous/script.js
@@ -3,6 +3,31 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+const renderError = function (msg) {
+   countriesContainer.insertAdjacentText('beforeend', msg);
+   // countriesContainer.style.opacity = 1;
+};
+
+const renderCountry = function (data, className = '') {
+   const langKey = Object.keys(data.languages)[0];
+   const curKey = Object.keys(data.currencies)[0];
+
+   const html = `
+   <article class="country ${className}">
+          <img class="country__img" src="${data.flags.svg}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name.common}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} people</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[langKey]}</p>
+            <p class="country__row"><span>💰</span>${data.currencies[curKey].name}</p>
+          </div>
+        </article>
+   `;
+   countriesContainer.insertAdjacentHTML('beforeend', html);
+   // countriesContainer.style.opacity = 1;
+};
+
 ///////////////////////////////////////
 /* 
 const getCountryData = function (country) {
@@ -43,25 +68,6 @@ getCountryData('germany');
 ////////////////////////////////////////////////////////////////////
 // Callback hell
 
-const renderCountry = function (data, className = '') {
-   const langKey = Object.keys(data.languages)[0];
-   const curKey = Object.keys(data.currencies)[0];
-
-   const html = `
-   <article class="country ${className}">
-          <img class="country__img" src="${data.flags.svg}" />
-          <div class="country__data">
-            <h3 class="country__name">${data.name.common}</h3>
-            <h4 class="country__region">${data.region}</h4>
-            <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} people</p>
-            <p class="country__row"><span>🗣️</span>${data.languages[langKey]}</p>
-            <p class="country__row"><span>💰</span>${data.currencies[curKey].name}</p>
-          </div>
-        </article>
-   `;
-   countriesContainer.insertAdjacentHTML('beforeend', html);
-   countriesContainer.style.opacity = 1;
-};
 /* 
 const getCountryAndNeighbour = function (country) {
    // AJAX call country 1
@@ -126,7 +132,15 @@ const getCountryData = function (country) {
          return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
       })
       .then(response => response.json())
-      .then(data => renderCountry(data[0], 'neighbour'));
+      .then(data => renderCountry(data[0], 'neighbour'))
+      .catch(err => {
+         console.error(err);
+         renderError(`Something went wrong: ${err.message}. Try again!`);
+      })
+      .finally(() => {
+         countriesContainer.style.opacity = 1;
+      });
 };
-
-getCountryData('portugal');
+btn.addEventListener('click', function () {
+   getCountryData('portugal');
+});
