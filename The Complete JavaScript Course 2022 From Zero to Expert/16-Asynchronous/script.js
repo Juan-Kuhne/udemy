b4835c403267ef69f1e85fcd4a66825535d80cae
Juan Kuhne <juan.kuhne@certisign.com.br>
2022-04-05 13:55:45 -0300

    Chaining promises

--- The Complete JavaScript Course 2022 From Zero to Expert/16-Asynchronous/script.js
+++ The Complete JavaScript Course 2022 From Zero to Expert/16-Asynchronous/script.js
@@ -118,7 +118,15 @@ getCountryAndNeighbour('german');
 const getCountryData = function (country) {
    fetch(`https://restcountries.com/v3.1/name/${country}`)
       .then(response => response.json())
-      .then(data => renderCountry(data[0]));
+      .then(data => {
+         renderCountry(data[0]);
+         const neighbour = data[0].borders[0];
+         if (!neighbour) return;
+
+         return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+      })
+      .then(response => response.json())
+      .then(data => renderCountry(data[0], 'neighbour'));
 };
 
 getCountryData('portugal');
