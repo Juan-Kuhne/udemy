c581ef1084f7c19cf1bfc49be99457b7fb1b0973
Juan Kuhne <juan.kuhne@certisign.com.br>
2022-04-06 10:07:37 -0300

    Throwing errors manually

--- The Complete JavaScript Course 2022 From Zero to Expert/16-Asynchronous/script.js
+++ The Complete JavaScript Course 2022 From Zero to Expert/16-Asynchronous/script.js
@@ -121,17 +121,22 @@ getCountryAndNeighbour('german');
 //       });
 // };
 
+const getJSON = function (url, errorMsg = 'Something went wrong ...') {
+   return fetch(url).then(response => {
+      if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+      return response.json();
+   });
+};
+
 const getCountryData = function (country) {
-   fetch(`https://restcountries.com/v3.1/name/${country}`)
-      .then(response => response.json())
+   getJSON(`https://restcountries.com/v3.1/name/${country}`, 'Country not found')
       .then(data => {
          renderCountry(data[0]);
+         if (!data[0].borders) throw new Error('Neighbour not found!');
          const neighbour = data[0].borders[0];
-         if (!neighbour) return;
 
-         return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+         return getJSON(`https://restcountries.com/v3.1/alpha/${neighbour}`, 'Country not found');
       })
-      .then(response => response.json())
       .then(data => renderCountry(data[0], 'neighbour'))
       .catch(err => {
          console.error(err);
@@ -142,5 +147,5 @@ const getCountryData = function (country) {
       });
 };
 btn.addEventListener('click', function () {
-   getCountryData('portugal');
+   getCountryData('australia');
 });
