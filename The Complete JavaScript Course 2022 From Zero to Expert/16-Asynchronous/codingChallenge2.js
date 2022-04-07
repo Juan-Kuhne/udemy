6d05f7cf40133a31886d573b9fedb07076669a9d
Juan Kuhne <juan.kuhne@certisign.com.br>
2022-04-07 15:45:50 -0300

    Section 16 Coding challenge 2

---
+++ The Complete JavaScript Course 2022 From Zero to Expert/16-Asynchronous/codingChallenge2.js
@@ -0,0 +1,36 @@
+const imgContainer = document.querySelector('.images');
+let curImg;
+
+const wait = function (seconds) {
+   return new Promise(function (resolve) {
+      setTimeout(resolve, seconds * 1000);
+   });
+};
+
+const createImage = function (imgPath) {
+   return new Promise(function (resolve, reject) {
+      const imgEl = document.createElement('img');
+      imgEl.src = imgPath;
+      imgEl.addEventListener('load', function (e) {
+         imgContainer.insertAdjacentElement('beforeend', imgEl);
+         resolve(this);
+      });
+      imgEl.addEventListener('error', () => reject('Error loading image'));
+   });
+};
+
+createImage('img/img-1.jpg')
+   .then(el => {
+      wait(2).then(() => {
+         el.style.display = 'none';
+         createImage('img/img-2.jpg').then(el => {
+            wait(2).then(() => {
+               el.style.display = 'none';
+               createImage('img/img-3.jpg').then(el => {
+                  wait(2).then(() => (el.style.display = 'none'));
+               });
+            });
+         });
+      });
+   })
+   .catch(err => console.error(err));
