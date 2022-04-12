3e8c121a552d746d077fb467a5475ca96e7965cb
Lucris <juankuhne@hotmail.com>
2022-04-12 10:52:01 -0300

    Other promise combinators: race allsettled and any

--- The Complete JavaScript Course 2022 From Zero to Expert/16-Asynchronous/script.js
+++ The Complete JavaScript Course 2022 From Zero to Expert/16-Asynchronous/script.js
@@ -286,14 +286,14 @@ console.log('1: Will get location');
 
 /////////////////////////////////////////////////////////////////////////
 // Running promises in parallel
-
+/* 
 const get3Countries = async function (c1, c2, c3) {
    try {
       // const [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
       // const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
       // const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
       // console.log(data1.capital, data2.capital, data3.capital);
-
+      
       const data = await Promise.all([
          getJSON(`https://restcountries.com/v3.1/name/${c1}`),
          getJSON(`https://restcountries.com/v3.1/name/${c2}`),
@@ -305,3 +305,37 @@ const get3Countries = async function (c1, c2, c3) {
    }
 };
 get3Countries('portugal', 'canada', 'tanzania');
+ */
+
+/////////////////////////////////////////////////////////////////////////
+// Other promise combinators: race, allsettled and any
+
+// Promise.race
+(async function () {
+   const res = await Promise.race([
+      getJSON(`https://restcountries.com/v3.1/name/italy`),
+      getJSON(`https://restcountries.com/v3.1/name/egypt`),
+      getJSON(`https://restcountries.com/v3.1/name/mexico`),
+   ]);
+   console.log(res[0]);
+})();
+
+const timeout = function (sec) {
+   return new Promise(function (_, reject) {
+      setTimeout(() => {
+         reject(new Error('Request took too long!'));
+      }, sec * 1000);
+   });
+};
+
+Promise.race([getJSON(`https://restcountries.com/v3.1/name/tanzania`), timeout(0.8)])
+   .then(res => console.log(res[0]))
+   .catch(err => console.error(err));
+
+// Promise.allSettled
+Promise.allSettled([Promise.resolve('Success'), Promise.reject('Error'), Promise.resolve('Another success')]).then(res => console.log(res));
+
+// Promise.any [ES2021]
+Promise.any([Promise.resolve('Success'), Promise.reject('Error'), Promise.resolve('Another success')])
+   .then(res => console.log(res))
+   .catch(err => console.error(err));
