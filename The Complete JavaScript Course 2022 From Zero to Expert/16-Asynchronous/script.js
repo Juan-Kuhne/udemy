1bfceb4002b62c2497a482aee183d15e10124ebc
Lucris <juankuhne@hotmail.com>
2022-04-12 10:29:55 -0300

    Running promises in parallel

--- The Complete JavaScript Course 2022 From Zero to Expert/16-Asynchronous/script.js
+++ The Complete JavaScript Course 2022 From Zero to Expert/16-Asynchronous/script.js
@@ -230,7 +230,7 @@ whereAmI(-33.933, 18.474);
 
 /////////////////////////////////////////////////////////////////////////
 // Async/Await
-
+/* 
 const getPosition = function () {
    return new Promise(function (resolve, reject) {
       navigator.geolocation.getCurrentPosition(resolve, reject);
@@ -282,3 +282,26 @@ console.log('1: Will get location');
    }
    console.log('3: Finished getting location');
 })();
+ */
+
+/////////////////////////////////////////////////////////////////////////
+// Running promises in parallel
+
+const get3Countries = async function (c1, c2, c3) {
+   try {
+      // const [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
+      // const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
+      // const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
+      // console.log(data1.capital, data2.capital, data3.capital);
+
+      const data = await Promise.all([
+         getJSON(`https://restcountries.com/v3.1/name/${c1}`),
+         getJSON(`https://restcountries.com/v3.1/name/${c2}`),
+         getJSON(`https://restcountries.com/v3.1/name/${c3}`),
+      ]);
+      console.log(data.map(d => d[0].capital[0]));
+   } catch (err) {
+      console.log(err);
+   }
+};
+get3Countries('portugal', 'canada', 'tanzania');
