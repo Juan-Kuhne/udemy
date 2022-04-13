bb7fba6f248c19b93d97b3a85c631d3492eef378
Lucris <juankuhne@hotmail.com>
2022-04-13 14:48:00 -0300

    The module pattern

--- The Complete JavaScript Course 2022 From Zero to Expert/17-Modern-JS-Modules-Tooling/script.js
+++ The Complete JavaScript Course 2022 From Zero to Expert/17-Modern-JS-Modules-Tooling/script.js
@@ -1,6 +1,6 @@
 // Importing module
 // import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
-console.log('Importing module');
+/* console.log('Importing module'); */
 // addToCart('bread', 5);
 // console.log(price, tq);
 
@@ -10,18 +10,18 @@ console.log('Importing module');
 
 // import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
 // console.log(price);
-
+/* 
 import add from './shoppingCart.js';
 add('pizza', 2);
 add('bread', 5);
 add('apple', 4);
-
+ */
 // console.log('Start fetching');
 // const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
 // const data = await res.json();
 // console.log(data);
 // console.log('Something');
-
+/* 
 const getLastPost = async function () {
    const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
    const data = await res.json();
@@ -36,4 +36,35 @@ console.log(lastPost);
 // lastPost.then(last => console.log(last));
 
 const lastPost2 = await getLastPost();
-console.log(lastPost2);
+console.log(lastPost2); */
+
+//////////////////////////////////////////////////////////////////////
+// The module pattern
+
+const ShoppingCart2 = (function () {
+   const cart = [];
+   const shippingCost = 10;
+   const totalPrice = 237;
+   const totalQuantity = 23;
+
+   const addToCart = function (product, quantity) {
+      cart.push({ product, quantity });
+      console.log(`${quantity} ${product} added to cart (sipping cost is ${shippingCost})`);
+   };
+
+   const orderStock = function (product, quantity) {
+      console.log(`${quantity} ${product} ordered from supplier`);
+   };
+
+   return {
+      addToCart,
+      cart,
+      totalPrice,
+      totalQuantity,
+   };
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizza', 2);
+console.log(ShoppingCart2);
+console.log(ShoppingCart2.shippingCost);
