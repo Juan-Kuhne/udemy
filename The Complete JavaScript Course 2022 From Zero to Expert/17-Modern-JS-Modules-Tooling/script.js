f2ead93329201bd6ee306b80ffe4e8b4f0401121
Lucris <juankuhne@hotmail.com>
2022-04-14 13:09:16 -0300

    Configuring babel and polyfilling

--- The Complete JavaScript Course 2022 From Zero to Expert/17-Modern-JS-Modules-Tooling/script.js
+++ The Complete JavaScript Course 2022 From Zero to Expert/17-Modern-JS-Modules-Tooling/script.js
@@ -95,3 +95,20 @@ console.log(stateDeepClone);
 if (module.hot) {
    module.hot.accept();
 }
+
+class Person {
+   greeting = 'Hey';
+   constructor(name) {
+      this.name;
+      console.log(`${this.greeting}, ${this.name}`);
+   }
+}
+
+const jonas = new Person('Jonas');
+console.log('Jonas' ?? null);
+
+console.log(cart.find(el => el.quantity >= 2));
+
+// import 'core-js/stable';
+import 'core-js/stable/array/find';
+import 'regenerator-runtime/runtime';
