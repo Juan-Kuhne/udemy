c78a1801b9bb0769c700e68603e23aaf84ad53a2
Lucris <juankuhne@hotmail.com>
2022-04-21 20:21:45 -0300

    implementing search results - part 1

--- The Complete JavaScript Course 2022 From Zero to Expert/18-forkify/src/js/model.js
+++ The Complete JavaScript Course 2022 From Zero to Expert/18-forkify/src/js/model.js
@@ -4,11 +4,15 @@ import { getJSON } from './helpers.js';
 
 export const state = {
   recipe: {},
+  search: {
+    query: '',
+    results: [],
+  },
 };
 
 export const loadRecipe = async function (id) {
   try {
-    const data = await getJSON(`${API_URL}/${id}`);
+    const data = await getJSON(`${API_URL}${id}`);
 
     //  const res = await fetch(`${API_URL}/${id}`);
     //  const data = await res.json();
@@ -34,3 +38,24 @@ export const loadRecipe = async function (id) {
     throw err;
   }
 };
+
+export const loadSearchResults = async function (query) {
+  try {
+    state.search.query = query;
+
+    const { data } = await getJSON(`${API_URL}?search=${query}`);
+
+    state.search.results = data.recipes.map(rec => {
+      return {
+        id: rec.id,
+        title: rec.title,
+        publisher: rec.publisher,
+        image: rec.image_url,
+      };
+    });
+  } catch (err) {
+    console.log(`${err} 💥💥`);
+    throw err;
+  }
+};
+loadSearchResults('pizza');
