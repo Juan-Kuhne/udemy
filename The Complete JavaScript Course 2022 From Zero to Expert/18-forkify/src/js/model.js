e304067878913e84895789fac0aae6ac43231f0b
Lucris <juankuhne@hotmail.com>
2022-04-24 00:10:19 -0300

    Implementing Bookmarks - Part 1

--- The Complete JavaScript Course 2022 From Zero to Expert/18-forkify/src/js/model.js
+++ The Complete JavaScript Course 2022 From Zero to Expert/18-forkify/src/js/model.js
@@ -10,6 +10,7 @@ export const state = {
     results: [],
     resultsPerPage: RES_PER_PAGE,
   },
+  bookmarks: [],
 };
 
 export const loadRecipe = async function (id) {
@@ -33,6 +34,10 @@ export const loadRecipe = async function (id) {
       cookingTime: recipe.cooking_time,
       ingredients: recipe.ingredients,
     };
+
+    if (state.bookmarks.some(bookmark => bookmark.id === id))
+      state.recipe.bookmarked = true;
+    else state.recipe.bookmarked = false;
   } catch (err) {
     // Temp error handling
     console.error(`💥 ${err}`);
@@ -73,4 +78,21 @@ export const updateServings = function (newServings) {
     ing.quantity *= newServings / state.recipe.servings;
   });
   state.recipe.servings = newServings;
+};
+
+export const addBookmark = function (recipe) {
+  // Add bookmark
+  state.bookmarks.push(recipe);
+
+  // Mark current recipe as bookmarked
+  if (recipe.id === state.recipe.id) state.recipe.bookmarked = true;
+};
+
+export const deleteBookmark = function (id) {
+  // Delete bookmark
+  const index = state.bookmarks.findIndex(bookmark => bookmark.id === id);
+  state.bookmarks.splice(index, 1);
+
+  // Mark current recipe as  NOT bookmarked
+  if (id === state.recipe.id) state.recipe.bookmarked = false;
 };
--- The Complete JavaScript Course 2022 From Zero to Expert/18-forkify/src/js/views/recipeView.js
+++ The Complete JavaScript Course 2022 From Zero to Expert/18-forkify/src/js/views/recipeView.js
@@ -27,6 +27,14 @@ class RecipeView extends View {
     });
   }
 
+  addHandlerAddBookmark(handler) {
+    this._parentElement.addEventListener('click', function (e) {
+      const btn = e.target.closest('.btn--bookmark');
+      if (!btn) return;
+      handler();
+    });
+  }
+
   _generateMarkup() {
     return `
    <figure class="recipe__fig">
@@ -77,9 +85,11 @@ class RecipeView extends View {
 
      <div class="recipe__user-generated">
      </div>
-     <button class="btn--round">
+     <button class="btn--round btn--bookmark">
        <svg class="">
-         <use href="${iconPath}#icon-bookmark-fill"></use>
+         <use href="${iconPath}#icon-bookmark${
+      this._data.bookmarked ? '-fill' : ''
+    }"></use>
        </svg>
      </button>
    </div>
