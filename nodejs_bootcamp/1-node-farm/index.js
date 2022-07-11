27d20358efcdac9707c6a453a3b45474f709b8b4
Lucris <juankuhne@hotmail.com>
2022-07-11 16:39:28 -0300

    Routing

--- nodejs_bootcamp/1-node-farm/index.js
+++ nodejs_bootcamp/1-node-farm/index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const http = require("http");
+const url = require("url");
 
 ///////////////////////////////////////////////////////////////////
 // FILES
@@ -32,7 +33,17 @@ const http = require("http");
 ///////////////////////////////////////////////////////////////////
 // SERVER
 const server = http.createServer((req, res) => {
-  res.end("Hello from the server!!!");
+  if (req.url === "/" || req.url === "/overview") {
+    res.end("This is the OVERVIEW !!");
+  } else if (req.url === "/product") {
+    res.end("This is the PRODUCT !!!");
+  } else {
+    res.writeHead("404", {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<h1>Page not found ...</h1>");
+  }
 });
 
 server.listen("8000", "127.0.0.1", () => {
