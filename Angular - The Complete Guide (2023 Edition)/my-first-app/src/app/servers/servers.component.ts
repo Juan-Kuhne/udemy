4c33e1224320ba0cf717701b8448acf9b4a480ef
Juan_Kuhne <juan.kuhne@certisign.com.br>
2022-11-24 14:07:02 -0300

    Using ngIf

--- Angular - The Complete Guide (2023 Edition)/my-first-app/src/app/servers/servers.component.ts
+++ Angular - The Complete Guide (2023 Edition)/my-first-app/src/app/servers/servers.component.ts
@@ -15,12 +15,14 @@ export class ServersComponent {
   allowNewServer = false;
   serverCreationStatus: string = 'No server was created!';
   serverName = 'TestServer';
+  serverCreated = false;
 
   constructor() {
     setTimeout(()=>{ this.allowNewServer = true }, 2000);
   }
 
   onCreateServer() {
+    this.serverCreated = true;
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
