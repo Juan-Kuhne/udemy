415da2680de28d4dcf8e889cfb3dd2dd617b41c1
juan-kuhne <juankuhne@hotmail.com>
2023-04-12 10:27:18 -0300

    building a very simple api

--- nodejs_bootcamp/1-node-farm/index.js
+++ nodejs_bootcamp/1-node-farm/index.js
@@ -32,11 +32,20 @@ const url = require("url");
 
 ///////////////////////////////////////////////////////////////////
 // SERVER
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
-  if (req.url === "/" || req.url === "/overview") {
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "/overview") {
     res.end("This is the OVERVIEW !!");
-  } else if (req.url === "/product") {
+  } else if (pathName === "/product") {
     res.end("This is the PRODUCT !!!");
+  } else if (pathName === '/api') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(data);
   } else {
     res.writeHead("404", {
       "Content-type": "text/html",
